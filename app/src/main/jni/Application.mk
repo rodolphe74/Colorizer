8c281e75474e4f199d544b6d5f5c4634c5ba4f7b
rodolphe74 <rcordenie@gmail.com>
2018-10-31 05:26:44 +0100

    Use of OpenCV 4

--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -1,4 +1,6 @@
-APP_STL := gnustl_static
+# APP_STL := gnustl_static
+# APP_STL := c++_static
+APP_STL := c++_shared
 APP_CPPFLAGS := -frtti -fexceptions
 APP_ABI := armeabi-v7a arm64-v8a x86
 APP_PLATFORM := android-8
